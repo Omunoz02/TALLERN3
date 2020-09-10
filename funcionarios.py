8327f5bdc35b9aaa08c502175107f47fcab4f475
rprojas <70291111+rprojas@users.noreply.github.com>
2020-09-10 00:53:49 -0500

    Update funcionarios.py

--- funcionarios.py
+++ funcionarios.py
@@ -22,4 +22,21 @@ def consultaFuncionario(funcionario):
     ID=int(input("\n Ingrese el ID del funcionario a consultar:\n"))
     if ID in funcionario:
         print(funcionario[ID][0],funcionario[ID][1],funcionario[ID][2])
-        
+   
+
+def eliminarfuncionario(funcionario):
+    
+    del funcionario[int(input("\n Ingrese el ID del funcionario a eliminar: \n "))]
+    print("\n Se ha eliminado \n  ")
+
+    print("\n Listado completo de Funcionarios:\n ")
+    for ID in funcionario:
+        print(ID,funcionario[ID][0],funcionario[ID][1],funcionario[ID][2])
+
+
+
+
+funcionario=AgregarFuncionario()
+imprimirLista(funcionario)
+consultaFuncionario(funcionario)
+eliminarfuncionario(funcionario)
